2d07452daf4b670c74c41ff672a78c28f09ba245
cliente <cliente@DESKTOP-HD05D0Q>
2020-06-01 21:29:19 -0600

    Acumulador Contador

---
+++ Ejercicios Clase/6_AcumuladorContador.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class _6_AcumuladorContador
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtPedido = New System.Windows.Forms.MaskedTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Txt_Individual = New System.Windows.Forms.TextBox()
+        Me.Txt_TotalPagar = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BtnSolicitarNuevo = New System.Windows.Forms.Button()
+        Me.BtnNuevo = New System.Windows.Forms.Button()
+        Me.BtnSalir = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtPedido)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(45, 54)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(271, 154)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Pedido"
+        '
+        'txtPedido
+        '
+        Me.txtPedido.Location = New System.Drawing.Point(130, 52)
+        Me.txtPedido.Name = "txtPedido"
+        Me.txtPedido.Size = New System.Drawing.Size(132, 25)
+        Me.txtPedido.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 60)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Orden Combos"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Txt_Individual)
+        Me.GroupBox2.Controls.Add(Me.Txt_TotalPagar)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(45, 214)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(271, 152)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Pago"
+        '
+        'Txt_Individual
+        '
+        Me.Txt_Individual.Location = New System.Drawing.Point(130, 93)
+        Me.Txt_Individual.Multiline = True
+        Me.Txt_Individual.Name = "Txt_Individual"
+        Me.Txt_Individual.Size = New System.Drawing.Size(118, 26)
+        Me.Txt_Individual.TabIndex = 3
+        '
+        'Txt_TotalPagar
+        '
+        Me.Txt_TotalPagar.Location = New System.Drawing.Point(130, 38)
+        Me.Txt_TotalPagar.Multiline = True
+        Me.Txt_TotalPagar.Name = "Txt_TotalPagar"
+        Me.Txt_TotalPagar.Size = New System.Drawing.Size(118, 26)
+        Me.Txt_TotalPagar.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 93)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 17)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Individual"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(93, 17)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Total a Pagar"
+        '
+        'BtnSolicitarNuevo
+        '
+        Me.BtnSolicitarNuevo.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.BtnSolicitarNuevo.Cursor = System.Windows.Forms.Cursors.Default
+        Me.BtnSolicitarNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnSolicitarNuevo.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSolicitarNuevo.Location = New System.Drawing.Point(371, 82)
+        Me.BtnSolicitarNuevo.Name = "BtnSolicitarNuevo"
+        Me.BtnSolicitarNuevo.Size = New System.Drawing.Size(106, 45)
+        Me.BtnSolicitarNuevo.TabIndex = 2
+        Me.BtnSolicitarNuevo.Text = "Solicitar Orden"
+        Me.BtnSolicitarNuevo.UseVisualStyleBackColor = False
+        '
+        'BtnNuevo
+        '
+        Me.BtnNuevo.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.BtnNuevo.Cursor = System.Windows.Forms.Cursors.Default
+        Me.BtnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnNuevo.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnNuevo.Location = New System.Drawing.Point(371, 167)
+        Me.BtnNuevo.Name = "BtnNuevo"
+        Me.BtnNuevo.Size = New System.Drawing.Size(106, 45)
+        Me.BtnNuevo.TabIndex = 3
+        Me.BtnNuevo.Text = "Nuevo"
+        Me.BtnNuevo.UseVisualStyleBackColor = False
+        '
+        'BtnSalir
+        '
+        Me.BtnSalir.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.BtnSalir.Cursor = System.Windows.Forms.Cursors.Default
+        Me.BtnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnSalir.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSalir.Location = New System.Drawing.Point(371, 359)
+        Me.BtnSalir.Name = "BtnSalir"
+        Me.BtnSalir.Size = New System.Drawing.Size(106, 45)
+        Me.BtnSalir.TabIndex = 4
+        Me.BtnSalir.Text = "Salir"
+        Me.BtnSalir.UseVisualStyleBackColor = False
+        '
+        '_6_AcumuladorContador
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(552, 459)
+        Me.Controls.Add(Me.BtnSalir)
+        Me.Controls.Add(Me.BtnNuevo)
+        Me.Controls.Add(Me.BtnSolicitarNuevo)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Name = "_6_AcumuladorContador"
+        Me.Text = "Acumulador Contador"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtPedido As MaskedTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Txt_Individual As TextBox
+    Friend WithEvents Txt_TotalPagar As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents BtnSolicitarNuevo As Button
+    Friend WithEvents BtnNuevo As Button
+    Friend WithEvents BtnSalir As Button
+End Class
---
+++ Ejercicios Clase/6_AcumuladorContador.vb
@@ -0,0 +1,32 @@
+﻿Public Class _6_AcumuladorContador
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles BtnSalir.Click
+        End
+    End Sub
+
+    Private Sub BtnSolicitarNuevo_Click(sender As Object, e As EventArgs) Handles BtnSolicitarNuevo.Click
+
+        Try
+            Dim precio, total, totalPagar, orden As Integer
+
+            orden = txtPedido.Text
+            For i = 1 To orden Step 1
+                precio = InputBox("Ingrsa el precio del platillo N." & i, "Precio del plantillo ")
+                'Acumulador: guarda los valores ingresados previamente 
+                totalPagar += precio
+            Next
+            total = totalPagar / orden
+            Txt_TotalPagar.Text = total
+            Txt_Individual.Text = total
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub BtnNuevo_Click(sender As Object, e As EventArgs) Handles BtnNuevo.Click
+        txtPedido.Clear()
+        Txt_TotalPagar.Clear()
+        Txt_Individual.Clear()
+
+    End Sub
+End Class
