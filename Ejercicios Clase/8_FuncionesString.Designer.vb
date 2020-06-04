7c2737b54c0a11173b8819c6d8384e24276352d2
cliente <cliente@DESKTOP-HD05D0Q>
2020-06-03 19:53:54 -0600

    Funciones String

---
+++ Ejercicios Clase/8_FuncionesString.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class _8_FuncionesString
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.BtnMostrar = New System.Windows.Forms.Button()
+        Me.Txt_Cadena = New System.Windows.Forms.TextBox()
+        Me.Txt_Longitud = New System.Windows.Forms.TextBox()
+        Me.Txt_UltimCaracter = New System.Windows.Forms.TextBox()
+        Me.Txt_PrimeCaracter = New System.Windows.Forms.TextBox()
+        Me.Txt_3a8 = New System.Windows.Forms.TextBox()
+        Me.Txt_PosicionB = New System.Windows.Forms.TextBox()
+        Me.Txt_Mayusculas = New System.Windows.Forms.TextBox()
+        Me.Txt_Minusculas = New System.Windows.Forms.TextBox()
+        Me.Txt_BorrarEspacios = New System.Windows.Forms.TextBox()
+        Me.BtnSalir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(101, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Cadena"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(101, 152)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(113, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Ultimo Caracter"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(101, 105)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(67, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Longitud"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(101, 192)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(114, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Primer Caracter"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(101, 367)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(83, 17)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Minusculas"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(101, 280)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(153, 17)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Posicion de la Letra B"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(101, 327)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(85, 17)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Mayusculas"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(101, 237)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(73, 17)
+        Me.Label8.TabIndex = 4
+        Me.Label8.Text = "3ro al 8vo"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(104, 408)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(114, 17)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "Borrar Espacios"
+        '
+        'BtnMostrar
+        '
+        Me.BtnMostrar.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.BtnMostrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnMostrar.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnMostrar.Location = New System.Drawing.Point(534, 89)
+        Me.BtnMostrar.Name = "BtnMostrar"
+        Me.BtnMostrar.Size = New System.Drawing.Size(92, 49)
+        Me.BtnMostrar.TabIndex = 9
+        Me.BtnMostrar.Text = "Mostrar"
+        Me.BtnMostrar.UseVisualStyleBackColor = False
+        '
+        'Txt_Cadena
+        '
+        Me.Txt_Cadena.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt_Cadena.Location = New System.Drawing.Point(287, 54)
+        Me.Txt_Cadena.Name = "Txt_Cadena"
+        Me.Txt_Cadena.Size = New System.Drawing.Size(141, 25)
+        Me.Txt_Cadena.TabIndex = 10
+        '
+        'Txt_Longitud
+        '
+        Me.Txt_Longitud.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt_Longitud.Location = New System.Drawing.Point(287, 97)
+        Me.Txt_Longitud.Name = "Txt_Longitud"
+        Me.Txt_Longitud.Size = New System.Drawing.Size(141, 25)
+        Me.Txt_Longitud.TabIndex = 11
+        '
+        'Txt_UltimCaracter
+        '
+        Me.Txt_UltimCaracter.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt_UltimCaracter.Location = New System.Drawing.Point(287, 144)
+        Me.Txt_UltimCaracter.Name = "Txt_UltimCaracter"
+        Me.Txt_UltimCaracter.Size = New System.Drawing.Size(141, 25)
+        Me.Txt_UltimCaracter.TabIndex = 12
+        '
+        'Txt_PrimeCaracter
+        '
+        Me.Txt_PrimeCaracter.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt_PrimeCaracter.Location = New System.Drawing.Point(287, 191)
+        Me.Txt_PrimeCaracter.Name = "Txt_PrimeCaracter"
+        Me.Txt_PrimeCaracter.Size = New System.Drawing.Size(141, 25)
+        Me.Txt_PrimeCaracter.TabIndex = 13
+        '
+        'Txt_3a8
+        '
+        Me.Txt_3a8.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt_3a8.Location = New System.Drawing.Point(287, 236)
+        Me.Txt_3a8.Name = "Txt_3a8"
+        Me.Txt_3a8.Size = New System.Drawing.Size(141, 25)
+        Me.Txt_3a8.TabIndex = 14
+        '
+        'Txt_PosicionB
+        '
+        Me.Txt_PosicionB.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt_PosicionB.Location = New System.Drawing.Point(287, 279)
+        Me.Txt_PosicionB.Name = "Txt_PosicionB"
+        Me.Txt_PosicionB.Size = New System.Drawing.Size(141, 25)
+        Me.Txt_PosicionB.TabIndex = 15
+        '
+        'Txt_Mayusculas
+        '
+        Me.Txt_Mayusculas.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt_Mayusculas.Location = New System.Drawing.Point(287, 326)
+        Me.Txt_Mayusculas.Name = "Txt_Mayusculas"
+        Me.Txt_Mayusculas.Size = New System.Drawing.Size(141, 25)
+        Me.Txt_Mayusculas.TabIndex = 16
+        '
+        'Txt_Minusculas
+        '
+        Me.Txt_Minusculas.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt_Minusculas.Location = New System.Drawing.Point(287, 366)
+        Me.Txt_Minusculas.Name = "Txt_Minusculas"
+        Me.Txt_Minusculas.Size = New System.Drawing.Size(141, 25)
+        Me.Txt_Minusculas.TabIndex = 17
+        '
+        'Txt_BorrarEspacios
+        '
+        Me.Txt_BorrarEspacios.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt_BorrarEspacios.Location = New System.Drawing.Point(287, 407)
+        Me.Txt_BorrarEspacios.Name = "Txt_BorrarEspacios"
+        Me.Txt_BorrarEspacios.Size = New System.Drawing.Size(141, 25)
+        Me.Txt_BorrarEspacios.TabIndex = 18
+        '
+        'BtnSalir
+        '
+        Me.BtnSalir.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.BtnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnSalir.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSalir.Location = New System.Drawing.Point(534, 191)
+        Me.BtnSalir.Name = "BtnSalir"
+        Me.BtnSalir.Size = New System.Drawing.Size(92, 49)
+        Me.BtnSalir.TabIndex = 19
+        Me.BtnSalir.Text = "Salir"
+        Me.BtnSalir.UseVisualStyleBackColor = False
+        '
+        '_8_FuncionesString
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(720, 521)
+        Me.Controls.Add(Me.BtnSalir)
+        Me.Controls.Add(Me.Txt_BorrarEspacios)
+        Me.Controls.Add(Me.Txt_Minusculas)
+        Me.Controls.Add(Me.Txt_Mayusculas)
+        Me.Controls.Add(Me.Txt_PosicionB)
+        Me.Controls.Add(Me.Txt_3a8)
+        Me.Controls.Add(Me.Txt_PrimeCaracter)
+        Me.Controls.Add(Me.Txt_UltimCaracter)
+        Me.Controls.Add(Me.Txt_Longitud)
+        Me.Controls.Add(Me.Txt_Cadena)
+        Me.Controls.Add(Me.BtnMostrar)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "_8_FuncionesString"
+        Me.Text = "Funciones String"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents BtnMostrar As Button
+    Friend WithEvents Txt_Cadena As TextBox
+    Friend WithEvents Txt_Longitud As TextBox
+    Friend WithEvents Txt_UltimCaracter As TextBox
+    Friend WithEvents Txt_PrimeCaracter As TextBox
+    Friend WithEvents Txt_3a8 As TextBox
+    Friend WithEvents Txt_PosicionB As TextBox
+    Friend WithEvents Txt_Mayusculas As TextBox
+    Friend WithEvents Txt_Minusculas As TextBox
+    Friend WithEvents Txt_BorrarEspacios As TextBox
+    Friend WithEvents BtnSalir As Button
+End Class
---
+++ Ejercicios Clase/8_FuncionesString.vb
@@ -0,0 +1,23 @@
+﻿Public Class _8_FuncionesString
+    Private Sub BtnMostrar_Click(sender As Object, e As EventArgs) Handles BtnMostrar.Click
+
+        Dim cadena As String
+        cadena = Txt_Cadena.Text
+        'length, retorna las posiciones deuna cadena incluye los espacios
+        Txt_Longitud.Text = cadena.Length
+
+        Txt_UltimCaracter.Text = cadena.Substring(cadena.Length - 1, 1)
+        Txt_PrimeCaracter.Text = cadena.Substring(0, 1)
+        Txt_3a8.Text = cadena.Substring(2, 7)
+        Txt_PosicionB.Text = cadena.IndexOf("B", 0) + 1
+        Txt_Mayusculas.Text = cadena.ToUpper
+        Txt_Minusculas.Text = cadena.ToLower
+        Txt_BorrarEspacios.Text = cadena.Replace(" ", "")
+
+    End Sub
+
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles BtnSalir.Click
+        End
+
+    End Sub
+End Class
